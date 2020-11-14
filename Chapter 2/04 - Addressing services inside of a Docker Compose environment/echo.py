2693047fa74631463b2594e2636e786f4228e859
Michał Jaworski <swistakm@gmail.com>
2020-11-14 17:31:49 +0100

    Chapter 2, Section: Adressing services inside of a Docker Compose environment

---
+++ Chapter 2/04 - Addressing services inside of a Docker Compose environment/echo.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def echo():
+    print(request.headers)
+    return (
+        f"METHOD: {request.method}\n"
+        f"HEADERS:\n{request.headers}"
+        f"BODY:\n{request.data.decode()}"
+    )
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
