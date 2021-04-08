c6ce3398a1b8c62c93d120992a55b10347b71807
Michał Jaworski <swistakm@gmail.com>
2021-04-09 01:50:08 +0200

    Chapter 12: sentry example

---
+++ Chapter 12/04 - Capturing errors for later review/sentry_example.py
@@ -0,0 +1,19 @@
+import logging
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,
+    event_level=logging.ERROR,
+)
+
+sentry_sdk.init(
+    dsn='https://<key>:<secret>@app.getsentry.com/<project>',
+    integrations=[sentry_logging],
+)
+
+try:
+    1 / 0
+except Exception as e:
+    sentry_sdk.capture_exception(e)
