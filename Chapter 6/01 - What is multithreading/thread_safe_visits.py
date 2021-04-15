19d08fbb48e39314398d27be2de48b0a93b979a6
Michał Jaworski <swistakm@gmail.com>
2021-04-15 10:54:33 +0200

    Chapter 6: new samples for presenting race conditions

---
+++ Chapter 6/01 - What is multithreading/thread_safe_visits.py
@@ -0,0 +1,26 @@
+from threading import Thread, Lock
+
+thread_visits = 0
+thread_visits_lock = Lock()
+
+
+def visit_counter():
+    global thread_visits
+    for i in range(100_000):
+        with thread_visits_lock:
+            thread_visits += 1
+
+
+if __name__ == "__main__":
+    thread_count = 100
+    threads = [
+        Thread(target=visit_counter)
+        for _ in range(thread_count)
+    ]
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(f"{thread_count=}, {thread_visits=}")
---
+++ Chapter 6/01 - What is multithreading/thread_visits.py
@@ -0,0 +1,26 @@
+from threading import Thread
+
+thread_visits = 0
+
+
+def visit_counter():
+    global thread_visits
+    for i in range(100_000):
+        # value = thread_visits
+        # thread_visits = value + 1
+        thread_visits += 1
+
+
+if __name__ == "__main__":
+    thread_count = 100
+    threads = [
+        Thread(target=visit_counter)
+        for _ in range(thread_count)
+    ]
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(f"{thread_count=}, {thread_visits=}")
